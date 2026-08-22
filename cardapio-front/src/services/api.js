dc2a6d42059b95b3357b2a111842433d306c85f0
Gabriel <gabrielalveslga1155@gmail.com>
2026-08-22 19:13:05 -0300

    Atualização

--- cardapio-front/src/services/api.js
+++ cardapio-front/src/services/api.js
@@ -5,7 +5,7 @@ const USER_KEY = "user";
 const ROLE_KEY = "role";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8081",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
